c8ad59a4c7d295c0f4862bdadb57204cfffd6a49
Gwendal Roué <gr@pierlis.com>
2017-08-29 13:38:55 +0200

    GRDatabaseTests setup

--- Sources/GRDatabase.swift
+++ Sources/GRDatabase.swift
@@ -7,11 +7,20 @@ import GRDB
         self.db = db
     }
     
+    @objc public func executeUpdate(_ sql: String) throws {
+        try db.execute(sql)
+    }
+    
     @objc public func executeUpdate(_ sql: String, values: [Any]?) throws {
         let arguments = values.map { StatementArguments(lossless: $0) }
         try db.execute(sql, arguments: arguments)
     }
     
+    @objc public func executeQuery(_ sql: String) throws -> GRResultSet {
+        let cursor = try Row.fetchCursor(db, sql)
+        return GRResultSet(cursor: cursor)
+    }
+
     @objc public func executeQuery(_ sql: String, values: [Any]?) throws -> GRResultSet {
         let arguments = values.map { StatementArguments(lossless: $0) }
         let cursor = try Row.fetchCursor(db, sql, arguments: arguments)
---
+++ Tests/GRDatabaseTests.m
@@ -0,0 +1,8 @@
+#import "GRDBObjcTestCase.h"
+
+@interface GRDatabaseTests : GRDBObjcTestCase
+@end
+
+@implementation GRDatabaseTests
+
+@end
