17cbcf1563d08d7b9f46bac704bb490c3ca749fa
Gwendal Roué <gr@pierlis.com>
2017-08-29 16:19:40 +0200

    GRResultSet value getters

--- Sources/GRResultSet.swift
+++ Sources/GRResultSet.swift
@@ -42,7 +42,22 @@ import GRDB
         }
     }
     
-    @objc(intForColumnIndex:) public func int(columnIndex: Int) -> CInt {
-        return row[columnIndex] ?? 0
-    }
+    @objc(intForColumnIndex:) public func int(columnIndex: Int) -> CInt { return row[columnIndex] ?? 0 }
+    @objc(intForColumn:) public func int(column: String) -> CInt { return row[column] ?? 0 }
+    @objc(longForColumnIndex:) public func long(columnIndex: Int) -> CLong { return row[columnIndex] ?? 0 }
+    @objc(longForColumn:) public func long(column: String) -> CLong { return row[column] ?? 0 }
+    @objc(longLongIntForColumnIndex:) public func long(columnIndex: Int) -> CLongLong { return row[columnIndex] ?? 0 }
+    @objc(longLongIntForColumn:) public func long(column: String) -> CLongLong { return row[column] ?? 0 }
+    @objc(unsignedLongLongIntForColumnIndex:) public func long(columnIndex: Int) -> CUnsignedLongLong { return row[columnIndex] ?? 0 }
+    @objc(unsignedLongLongIntForColumn:) public func long(column: String) -> CUnsignedLongLong { return row[column] ?? 0 }
+    @objc(boolForColumnIndex:) public func bool(columnIndex: Int) -> Bool { return row[columnIndex] ?? false }
+    @objc(boolForColumn:) public func bool(column: String) -> Bool { return row[column] ?? false }
+    @objc(doubleForColumnIndex:) public func double(columnIndex: Int) -> Double { return row[columnIndex] ?? 0.0 }
+    @objc(doubleForColumn:) public func double(column: String) -> Double { return row[column] ?? 0.0 }
+    @objc(stringForColumnIndex:) public func string(columnIndex: Int) -> String? { return row[columnIndex] }
+    @objc(stringForColumn:) public func string(column: String) -> String? { return row[column] }
+    @objc(dataForColumnIndex:) public func data(columnIndex: Int) -> Data? { return row[columnIndex] }
+    @objc(dataForColumn:) public func data(column: String) -> Data? { return row[column] }
+    @objc(objectForColumnIndex:) public func object(columnIndex: Int) -> Any? { return row[columnIndex] }
+    @objc(objectForColumn:) public func object(column: String) -> Any? { return row[column] }
 }
