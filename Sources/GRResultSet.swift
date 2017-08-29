d5f807142050380bc24b8478af819243e15533d5
Gwendal Roué <gr@pierlis.com>
2017-08-29 13:38:42 +0200

    GRResultSet cleanup

--- Sources/GRResultSet.swift
+++ Sources/GRResultSet.swift
@@ -1,33 +1,48 @@
 import GRDB
 
 @objc public class GRResultSet : NSObject {
-    let cursor: RowCursor
-    var row: Row?
-    
+    private let cursor: RowCursor
+    private enum State {
+        case initialized
+        case row(Row)
+        case ended
+        case error(Error)
+    }
+    private var state = State.initialized
+    private var row: Row {
+        switch state {
+        case .initialized: fatalError("-[GRResultSet next] has to be called before accessing fetched results")
+        case .row(let row): return row
+        case .ended: fatalError("GRResultSet has been fully consumed")
+        case .error(let error): fatalError("GRResultSet had an error: \(error)")
+        }
+    }
+
     init(cursor: RowCursor) {
         self.cursor = cursor
-        self.row = nil
     }
     
     @objc public func next() -> Bool {
-        do {
-            if let fetchedRow = try cursor.next() {
-                row = fetchedRow
-                return true
-            } else {
-                row = nil
+        switch state {
+        case .initialized, .row:
+            do {
+                if let row = try cursor.next() {
+                    state = .row(row)
+                    return true
+                } else {
+                    state = .ended
+                    return false
+                }
+            } catch {
+                state = .error(error)
                 return false
             }
-        } catch {
-            // TODO: handle error
-            return false
+        case .ended: return false
+        case .error: return false
         }
     }
     
     @objc(intForColumnIndex:) public func int(columnIndex: Int) -> CInt {
-        guard let row = row else {
-            fatalError("-[GRResultSet next] has to be called before accessing fetched results")
-        }
         return row[columnIndex]
     }
 }
