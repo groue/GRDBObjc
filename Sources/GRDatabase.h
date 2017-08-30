a6618f25d08903621e5958d908b7a789499a1c91
Gwendal Roué <gr@pierlis.com>
2017-08-30 08:21:46 +0200

    Comment out -[GRDatabase executeUpdate:withErrorAndBindings:]

--- Sources/GRDatabase.h
+++ Sources/GRDatabase.h
@@ -1,6 +1,7 @@
-#import <GRDBObjc/GRDBObjc-Swift.h>
+//#import <GRDBObjc/GRDBObjc-Swift.h>
+//
+//@interface GRDatabase(Objc)
+//// TODO: test
+//- (BOOL)executeUpdate:(NSString * _Nonnull)sql withErrorAndBindings:(NSError * _Nullable * _Nullable)outErr, ...;
+//@end
 
-@interface GRDatabase(Objc)
-// TODO: test
-- (BOOL)executeUpdate:(NSString * _Nonnull)sql withErrorAndBindings:(NSError * _Nullable * _Nullable)outErr, ...;
-@end
