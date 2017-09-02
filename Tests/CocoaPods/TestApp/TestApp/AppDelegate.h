d838f843c477cb75af132af8545cf8726571930a
Gwendal Roué <gr@pierlis.com>
2017-09-02 13:05:15 +0200

    CocoaPods test app (requires SWIFT_VERSION 4.0 in host app)

---
+++ Tests/CocoaPods/TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Gwendal Roué on 02/09/2017.
+//  Copyright © 2017 Gwendal Roué. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tests/CocoaPods/TestApp/TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Gwendal Roué on 02/09/2017.
+//  Copyright © 2017 Gwendal Roué. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
