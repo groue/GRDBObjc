4e12eed067def6d23524a10bdb05e1ddf0675981
Gwendal Roué <gr@pierlis.com>
2018-08-21 20:52:59 +0200

    @autoreleasepool requires strong references to returned values

--- Sources/GRDBObjc/FMDatabase+Objc.m
+++ Sources/GRDBObjc/FMDatabase+Objc.m
@@ -23,19 +23,27 @@
 
 - (BOOL)executeUpdate:(NSString * _Nonnull)sql va_list:(va_list)args error:(NSError **)outErr;
 {
+    BOOL result = NO;
+    NSError *error = nil;
+    
+    // @autoreleasepool asserts intermediate GRDB values are deinited on the
+    // current (correct) thread.
     @autoreleasepool {
-        _FMUpdateStatement *statement = [self _makeUpdateStatement:sql error:outErr];
-        if (!statement) {
-            return NO;
+        _FMUpdateStatement *statement = [self _makeUpdateStatement:sql error:&error];
+        if (statement) {
+            NSMutableArray *arguments = [NSMutableArray array];
+            for(int i = sqlite3_bind_parameter_count(statement.sqliteHandle); i > 0; i--) {
+                id obj = va_arg(args, id);
+                [arguments addObject:obj ?: [NSNull null]];
+            }
+            result = [statement executeWithValues:arguments error:&error];
         }
-        
-        NSMutableArray *arguments = [NSMutableArray array];
-        for(int i = sqlite3_bind_parameter_count(statement.sqliteHandle); i > 0; i--) {
-            id obj = va_arg(args, id);
-            [arguments addObject:obj ?: [NSNull null]];
-        }
-        return [statement executeWithValues:arguments error:outErr];
     }
+    
+    if (result) { return result; }
+    NSParameterAssert(error);
+    if (outErr != NULL) { *outErr = error; }
+    return NO;
 }
 
 - (FMResultSet * _Nullable)executeQuery:(NSString * _Nonnull)sql, ...
@@ -49,19 +57,27 @@
 
 - (FMResultSet * _Nullable)executeQuery:(NSString * _Nonnull)sql va_list:(va_list)args error:(NSError **)outErr;
 {
+    FMResultSet *result = nil;
+    NSError *error = nil;
+    
+    // @autoreleasepool asserts intermediate GRDB values are deinited on the
+    // current (correct) thread.
     @autoreleasepool {
         _FMSelectStatement *statement = [self _makeSelectStatement:sql error:outErr];
-        if (!statement) {
-            return nil;
+        if (statement) {
+            NSMutableArray *arguments = [NSMutableArray array];
+            for(int i = sqlite3_bind_parameter_count(statement.sqliteHandle); i > 0; i--) {
+                id obj = va_arg(args, id);
+                [arguments addObject:obj ?: [NSNull null]];
+            }
+            result = [statement executeWithValues:arguments error:outErr];
         }
-        
-        NSMutableArray *arguments = [NSMutableArray array];
-        for(int i = sqlite3_bind_parameter_count(statement.sqliteHandle); i > 0; i--) {
-            id obj = va_arg(args, id);
-            [arguments addObject:obj ?: [NSNull null]];
-        }
-        return [statement executeWithValues:arguments error:outErr];
     }
+    
+    if (result) { return result; }
+    NSParameterAssert(error);
+    if (outErr != NULL) { *outErr = error; }
+    return nil;
 }
 
 @end
